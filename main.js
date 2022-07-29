a4987f233ac8e2a41ad44664a67d5f8cd1d7b238
MaheshRath08 <maheshrath.2002@gmail.com>
2022-07-29 23:04:26 +0530

    minor changes

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ addEl.addEventListener("click", ()=>{
     if(amountEl.value && contextEl.value && checkAmount()){
         amount += parseFloat(amountEl.value)
         updateAmount()
+        //added an class n to make it look green
         listEl.innerHTML += `<li class="p"><span class="left">${contextEl.value}</span>  <span class="right">${amountEl.value}</span></li>`
     }
     amountEl.value = ""
@@ -21,12 +22,13 @@ lessEl.addEventListener("click", ()=>{
     if(amountEl.value && contextEl.value && checkAmount()){
         amount -= parseFloat(amountEl.value)
         updateAmount()
+        //added an class n to make it look red
         listEl.innerHTML += `<li class="n"><span class="left">${contextEl.value}</span>  <span class="right">${amountEl.value}</span></li>`
     }
     amountEl.value = ""
     contextEl.value = ""  
 })
-
+//updates the amount on display with colors for positive and negetive
 function updateAmount(){
     result.innerHTML = amount
     if(amount<0){
@@ -35,7 +37,7 @@ function updateAmount(){
         result.style.color = "green"
     }
 }
-
+//checks amount to make sure if it's a number 
 function checkAmount(){
     if(isNaN(amountEl.value)){
         alert("The amount must be written in numbers only!!")
